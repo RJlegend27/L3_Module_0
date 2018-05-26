42a8e37ff6d3a358b040f281bb0b71eb4f49a458
RJlegend27 <league@WTS5.attlocal.net>
2018-05-26 14:28:51 -0700

    commit

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,33 +1,61 @@
 package IntroToArrayLists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
+public class GuestBook implements ActionListener {
+	// Create a GUI with two buttons. One button reads "Add Name" and the other
+	// button reads "View Names".
+	// When the add name button is clicked, display an input dialog that asks the
+	// user to enter a name. Add
+	// that name to an ArrayList. When the "View Names" button is clicked, display a
+	// message dialog that displays
 	// all the names added to the list. Format the list as follows:
+	JButton buttonA = new JButton("Add Name");
+	JButton buttonB = new JButton("View Names");
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	
 	public static void main(String[] args) {
 		GuestBook book = new GuestBook();
-		if()
+		ArrayList<String> names = new ArrayList<String>();
 		
+
+
 	}
-	 void createGUI(){
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		JButton button = new JButton("Add Name");
-		JButton buttonA = new JButton("View Names");
+
+	void createGUI() {
+		buttonA.addActionListener(this);
+		buttonB.addActionListener(this);
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(buttonA);
+		panel.add(buttonB);
+		frame.setSize(500, 500);
+		panel.setVisible(true);
 	}
-	
-	
-	
-	
-	
-	
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners 
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == buttonA) {
+			String userInput = JOptionPane.showInputDialog("Enter A Name");
+		}
+
+		if (e.getSource() == buttonB) {
+			
+			System.out.println();
+			
+			// Guest #1: Bob Banders
+			// Guest #2: Sandy Summers
+			// Guest #3: Greg Ganders
+			// Guest #4: Donny Doners
+		}
+	}
 }
