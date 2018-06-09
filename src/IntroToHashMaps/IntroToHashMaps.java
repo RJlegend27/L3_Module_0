2804b93506bc077c75764cae7659b62f40a6dec1
RJlegend27 <League@AdministorsiMac.attlocal.net>
2018-06-09 14:27:17 -0700

    Is this loss?

--- src/IntroToHashMaps/IntroToHashMaps.java
+++ src/IntroToHashMaps/IntroToHashMaps.java
@@ -1,16 +1,41 @@
 package IntroToHashMaps;
 
+
+import java.util.HashMap;
+
 public class IntroToHashMaps {
-	//1. Create a HashMap called roots with Integers for the keys and Doubles for the values.
-	
-	//2. Using a for-loop, add 500 entries to your HashMap. The key entry will be the 
-	//   current iteration of the loop (i). The value entry will be the square root of i.
-	
-	//3. Iterate through all the entries in your HashMap displaying the keys with their respective
-	//   square roots (values). Use the following format.
-	
-	//   The square Root of 0 is 0.0
-	//   The square Root of 1 is 1.0
-	//   The square Root of 2 is 1.4142135623730951
-	//   The square Root of 3 is 1.7320508075688772
+	public static void main(String[] args) {
+
+		// 1. Create a HashMap called roots with Integers for the keys and Doubles for
+		// the values.
+		HashMap<Integer, Double> roots = new HashMap<Integer, Double>();
+		roots.put(0, 0.0);
+		roots.put(1, 1.0);
+		roots.put(2, 1.4142135623730951);
+		roots.put(3, 1.7320508075688772);
+		// 2. Using a for-loop, add 500 entries to your HashMap. The key entry will be
+		// the
+		// current iteration of the loop (i). The value entry will be the square root of
+		// i.
+		for(int i = 0; i>500; i++) {
+			System.out.println(roots);
+		}
+		
+		System.out.println("\nAll the keys:");
+		for(Integer i : roots.keySet()){
+			System.out.println(i);
+		}
+		System.out.println("All the Values:");
+		for(Double d : roots.values()) {
+			System.out.println(d);
+		}
+		// 3. Iterate through all the entries in your HashMap displaying the keys with
+		// their respective
+		// square roots (values). Use the following format.
+		
+		// The square Root of 0 is 0.0
+		// The square Root of 1 is 1.0
+		// The square Root of 2 is 1.4142135623730951
+		// The square Root of 3 is 1.7320508075688772
+	}
 }
--- src/IntroToHashMaps/LogSearch.java
+++ src/IntroToHashMaps/LogSearch.java
@@ -1,31 +1,89 @@
 package IntroToHashMaps;
 
-public class LogSearch {
-  /* 
-	 * Crate a HashMap of Integers for the keys and Strings for the values.
-	 * Create a GUI with three buttons. 
-	 * Button 1: Add Entry
-	 * 				When this button is clicked, use an input dialog to ask the user to enter an ID number.
-	 * 				After an ID is entered, use another input dialog to ask the user to enter a name.
-	 * 				Add this information as a new entry to your HashMap.
-	 * 
-	 * Button 2: Search by ID
-	 * 				When this button is clicked, use an input dialog to ask the user to enter an ID number.
-	 * 				If that ID exists, display that name to the user.
-	 * 				Otherwise, tell the user that that entry does not exist.
-	 * 
-	 * Button 3: View List
-	 * 				When this button is clicked, display the entire list in a message dialog in the following format:
-	 * 				ID: 123  Name: Harry Howard
-	 * 				ID: 245  Name: Polly Powers
-	 * 				ID: 433  Name: Oliver Ortega
-	 * 				etc...
-	 * 
-	 * When this is complete, add a fourth button to your window.
-	 * Button 4: Remove Entry
-	 * 				When this button is clicked, prompt the user to enter an ID using an input dialog.
-	 * 				If this ID exists in the HashMap, remove it. Otherwise, notify the user that the ID
-	 * 				is not in the list. 
-	 *
-	 * */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class LogSearch implements ActionListener {
+	JButton button1 = new JButton("Create");
+	JButton button2 = new JButton("Search");
+	JButton button3 = new JButton("List");
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+
+	HashMap<Integer, String> log = new HashMap<Integer, String>();
+
+	void createGUI() {
+		frame.add(panel);
+		frame.setSize(1000, 1000);
+		frame.setVisible(true);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		panel.setVisible(true);
+		button1.setVisible(true);
+		button2.setVisible(true);
+		button3.setVisible(true);
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
+
+	}
+
+	public static void main(String[] args) {
+		LogSearch search = new LogSearch();
+		search.createGUI();
+		/*
+		 * Crate a HashMap of Integers for the keys and Strings for the values.
+		 */
+		/*
+		 * Create a GUI with three buttons. Button 1: Add Entry When this button is
+		 * clicked, use an input dialog to ask the user to enter an ID number. After an
+		 * ID is entered, use another input dialog to ask the user to enter a name. Add
+		 * this information as a new entry to your HashMap.
+		 * 
+		 * Button 2: Search by ID When this button is clicked, use an input dialog to
+		 * ask the user to enter an ID number. If that ID exists, display that name to
+		 * the user. Otherwise, tell the user that that entry does not exist.
+		 * 
+		 * Button 3: View List When this button is clicked, display the entire list in a
+		 * message dialog in the following format: ID: 123 Name: Harry Howard ID: 245
+		 * Name: Polly Powers ID: 433 Name: Oliver Ortega etc...
+		 * 
+		 * When this is complete, add a fourth button to your window. Button 4: Remove
+		 * Entry When this button is clicked, prompt the user to enter an ID using an
+		 * input dialog. If this ID exists in the HashMap, remove it. Otherwise, notify
+		 * the user that the ID is not in the list.
+		 *
+		 */
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String userInput;
+		String userName;
+		String userEntry;
+		if (e.getSource() == button1) {
+			userInput = JOptionPane.showInputDialog(null, "Enter a ID number");
+			userName = JOptionPane.showInputDialog(null, "Enter a name");
+			Integer.parseInt(userInput);
+			log.put(Integer.parseInt(userInput), userName);
+		}
+		if (e.getSource() == button2) {
+			userEntry = JOptionPane.showInputDialog(null, "Enter a ID number");
+			int Entry = Integer.parseInt(userEntry);
+			if (log.containsKey(Entry)) {
+				System.out.println(log.get(Entry));
+			} else { JOptionPane.showMessageDialog(null, "That entry does not exist");
+			}
+		}if(e.getSource() == button3) {
+			System.out.println(log);
+		}
+	}
 }
