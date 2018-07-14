3449e6a2b4fe0a4c47ea8002dc82a0c1c6b5ebfa
RJlegend27 <league@WorthyMini-5.attlocal.net>
2018-07-14 14:28:10 -0700

    text

--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,17 +1,52 @@
 package IntroToStacks;
 
-public class TextUndoRedo {
-	/* 
-	 * Create a JFrame with a JPanel and a JLabel.
-	 * 
-	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
-	 * 
-	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TextUndoRedo implements KeyListener {
+	/* Create a JFrame with a JPanel and a JLabel.
+	 */ 
+	 JFrame frame = new JFrame();
+	 JPanel panel = new JPanel();
+	 JLabel label = new JLabel();
+	 TextUndoRedo(){
+		 frame.addKeyListener(this);
+	 }
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		char letter=e.getKeyChar(); 
+		
+		label.setText(letter+"");
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode()==KeyEvent.VK_BACK_SPACE) {
+			
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	 
+	 /* Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
+	 */ 
+	 
+	 /* Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
 	 * Save that deleted character onto a Stack of Characters.
-	 * 
-	 * Choose a key to be the Undo key. Make it so that when that key is pressed, the top Character is popped 
+	 */
+	 
+	 /* Choose a key to be the Undo key. Make it so that when that key is pressed, the top Character is popped 
 	 * off the Stack and added back to the JLabel.
-	 * 
-	 * */
+	 */ 
+	 
 	
 }
