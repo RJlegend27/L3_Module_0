4bcba828a03e12a5733f03cee100dc4378653b31
YASHWINLM <league@WTS-IM-17.attlocal.net>
2018-08-25 14:25:54 -0700

    8/25/18

--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -3,6 +3,8 @@ package IntroToStacks;
 import java.util.Random;
 import java.util.Stack;
 
+import javax.swing.JOptionPane;
+
 public class IntroToStack {
 	public static void main(String[] args) {
 	Random gen = new Random();
@@ -14,13 +16,27 @@ public class IntroToStack {
 		for(int i = 0; i<101; i++){
 			double d = gen.nextDouble()*100;
 			doubles.push(d);
-			System.out.println(d);
+			//System.out.println(d);
 		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+JOptionPane.showMessageDialog(null,"Enter two numbers between 0-100");
+	
+		String userInput = JOptionPane.showInputDialog("Enter number between 0-99");
+		String secondUserInput = JOptionPane.showInputDialog("Enter a number "+ userInput+ "-100");
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
+		double min = Double.parseDouble(userInput);
+		double max = Double.parseDouble(secondUserInput);
 		
+		for(int i = 0; i<101; i++) {
+			double value = doubles.pop();
+//			System.out.println(doubles.pop());
+			if(min < value && max > value) {
+				System.out.println(value);
+			}
+		}
+		
+	
 		//   EXAMPLE:
 		//   NUM 1: 65
 		//   NUM 2: 75
