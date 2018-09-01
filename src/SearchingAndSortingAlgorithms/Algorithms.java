af6003d24d92736bb3035744e38ffb4a5dfe90ca
RJlegend27 <league@WTS5.attlocal.net>
2018-09-01 14:24:29 -0700

    9/1/18

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0; //<- this needs changing
+		return 1; //<- this needs changing
 	}
 	
 	//Add other methods here
