90aa7f2d0d37f98b4f891569e55cb8554f53c17c
RJlegend27 <league@WTS5.attlocal.net>
2018-09-15 14:24:33 -0700

    ArrayList New Modules

---
+++ src/Level_3_Module_1/Algorithms.java
@@ -0,0 +1,19 @@
+package Level_3_Module_1;
+
+import java.util.ArrayList;
+
+public class Algorithms {
+public static void main(String[] args) {
+	ArrayList <String> name = new ArrayList<>();
+	ArrayList <String> number = new ArrayList<>();
+	name.add("Aaron");
+	name.add("Abraham");
+	name.add("Adam");
+	number.add("1");
+	number.add("2");
+	number.add("3");
+	System.out.println(name);
+	System.out.println(number);
+	
+}
+}
